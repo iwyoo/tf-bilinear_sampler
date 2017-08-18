wrong order

bilinear_sampler.py
@@ -54,8 +54,8 @@ def bilinear_sampler(x, v, resize=False, normalize=False, crop=None):
   vy1 = vy0 + 1 # [N, H, W, 1]
 
   v00 = tf.concat([vy0, vx0], 3)
-  v01 = tf.concat([vy0, vx1], 3)
-  v10 = tf.concat([vy1, vx0], 3)
+  v01 = tf.concat([vy1, vx0], 3)
+  v10 = tf.concat([vy0, vx1], 3)
   v11 = tf.concat([vy1, vx1], 3) # [N, H, W, 2]
 
   padding = [[0, 0], [0, 0], [0, 0], [1, 0]]