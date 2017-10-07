090434ce5d28495135fc6eb4a4c2de265024fc92
iwyoo <doveu1234@gmail.com>
2017-10-07 14:35:47 +0900

    dynamic shape

--- bilinear_sampler.py
+++ bilinear_sampler.py
@@ -19,16 +19,20 @@ def bilinear_sampler(x, v, resize=False, normalize=False, crop=None, out="CONSTA
   """
 
   def _get_grid_array(N, H, W, h, w):
-    N_i = np.arange(N)
-    H_i = np.arange(h+1, h+H+1)
-    W_i = np.arange(w+1, w+W+1)
-    n, h, w, = np.meshgrid(N_i, H_i, W_i, indexing='ij')
-    n = np.expand_dims(n, axis=3) # [N, H, W, 1]
-    h = np.expand_dims(h, axis=3) # [N, H, W, 1]
-    w = np.expand_dims(w, axis=3) # [N, H, W, 1]
+    N_i = tf.range(N)
+    H_i = tf.range(h+1, h+H+1)
+    W_i = tf.range(w+1, w+W+1)
+    n, h, w, = tf.meshgrid(N_i, H_i, W_i, indexing='ij')
+    n = tf.expand_dims(n, axis=3) # [N, H, W, 1]
+    h = tf.expand_dims(h, axis=3) # [N, H, W, 1]
+    w = tf.expand_dims(w, axis=3) # [N, H, W, 1]
+    n = tf.cast(n, tf.float32) # [N, H, W, 1]
+    h = tf.cast(h, tf.float32) # [N, H, W, 1]
+    w = tf.cast(w, tf.float32) # [N, H, W, 1]
+
     return n, h, w
 
-  shape = x.get_shape().as_list() # Should it be fixed size ?
+  shape = tf.shape(x) # TRY : Dynamic shape
   N = shape[0]
   if crop is None:
     H_ = H = shape[1]
@@ -67,10 +71,13 @@ def bilinear_sampler(x, v, resize=False, normalize=False, crop=None, out="CONSTA
   vx1 = vx0 + 1
   vy1 = vy0 + 1 # [N, H, W, 1]
 
-  iy0 = tf.clip_by_value(vy0 + h, 0., H_+1)
-  iy1 = tf.clip_by_value(vy1 + h, 0., H_+1)
-  ix0 = tf.clip_by_value(vx0 + w, 0., W_+1)
-  ix1 = tf.clip_by_value(vx1 + w, 0., W_+1)
+  H_1 = tf.cast(H_+1, tf.float32)
+  W_1 = tf.cast(W_+1, tf.float32)
+  iy0 = tf.clip_by_value(vy0 + h, 0., H_1)
+  iy1 = tf.clip_by_value(vy1 + h, 0., H_1)
+  ix0 = tf.clip_by_value(vx0 + w, 0., W_1)
+  ix1 = tf.clip_by_value(vx1 + w, 0., W_1)
+
   i00 = tf.concat([n, iy0, ix0], 3)
   i01 = tf.concat([n, iy1, ix0], 3)
   i10 = tf.concat([n, iy0, ix1], 3)
